dd579fbc8d8f4c251864f28f8d345d2620906068
Dan Butler <daniel.jaffe.butler@gmail.com>
2014-05-20 22:37:00 -0700

    added rviz_record_object_pose and rviz_arrows

---
+++ rosbuild/rviz_arrows/build/CMakeFiles/rviz_arrows.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rviz_arrows.dir/src/rviz_arrows.cpp.o"
+  "../bin/rviz_arrows.pdb"
+  "../bin/rviz_arrows"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rviz_arrows.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ rosbuild/rviz_record_object_pose/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/test_results"
+export CMAKE_PREFIX_PATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/djbutler/rosbuild_ws/rviz_record_object_pose:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
+export ROS_TEST_RESULTS_DIR="/home/djbutler/rosbuild_ws/rviz_record_object_pose/build/test_results"
