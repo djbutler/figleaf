941156c42e76922aa1549214922b08ed7eb34877
Dan Butler <daniel.jaffe.butler@gmail.com>
2014-07-02 15:41:05 -0700

    Added rviz config files and start scripts

---
+++ scripts/rviz_boxes.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+roslaunch pr2_pbd_interaction pbd_demo_desktop.launch
---
+++ scripts/rviz_kinect_clean.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+./scripts/pr2_interactive_manipulation.sh ${HOME}/figleaf/rviz_configs/kinect_and_clean_nobuttons.rviz
