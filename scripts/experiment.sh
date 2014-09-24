f9cabaeb2b960562b7e04ba045e174ddb36a1172
Justin Huang <jstn@cs.washington.edu>
2014-09-24 16:59:50 -0700

    Clean up roslaunch nodes.

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -9,5 +9,18 @@ if [ -e $DATA_DIR/$1_$2.bag ]
 then
   echo "/home/$USER/experiment_data/$1_$2.bag already exists! Stopping launch"
 else
-  roslaunch figleaf_2d experiment.launch user:=$1 filter:=$2 data_dir:=$DATA_DIR
+  case "$2" in
+    clean)
+      roslaunch figleaf_2d experiment.launch user:=$1 data_dir:=$DATA_DIR filter:=$2 clean:=True
+      ;;
+    blur)
+      roslaunch figleaf_2d experiment.launch user:=$1 data_dir:=$DATA_DIR filter:=$2 blur:=True
+      ;;
+    mid)
+      roslaunch figleaf_2d experiment.launch user:=$1 data_dir:=$DATA_DIR filter:=$2 mid:=True
+      ;;
+    box)
+      roslaunch figleaf_2d experiment.launch user:=$1 data_dir:=$DATA_DIR filter:=$2 box:=True
+      ;;
+  esac
 fi
